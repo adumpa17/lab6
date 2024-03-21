5943c374e0349163e0b25e196e68aa9f1dda9a67
adumpa17 <adumpa17@gmail.com>
2024-03-20 20:42:31 -0400

    Did main

---
+++ main.py
@@ -0,0 +1,32 @@
+def encode(tempPassword):  # This is Abhiram Dumpa's code
+    encryptedPassword = ""
+    for i in range(0, len(tempPassword)):
+        tempNum = int(tempPassword[i])
+        tempNum += 3
+        if tempNum > 9:
+            tempNum -= 10
+        encryptedPassword += str(tempNum)
+    return encryptedPassword
+
+
+def decode(password):
+
+
+
+if __name__ == "__main__":
+    notDone = True
+    while notDone:
+        print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
+        choice = int(input("Please enter an option: "))
+
+        if choice == 1:
+            temp = input("Please enter your password to encode: ")
+            newPass = encode(temp)
+            print("Your password has been encoded and stored!")
+
+        if choice == 2:
+            tempDecode = decode(newPass)
+            print(f"The encoded password is {newPass}, and the original password is {tempDecode}.")
+
+        if choice == 3:
+            break
